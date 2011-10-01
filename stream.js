d71120fd3f2ffe9f8a12c396e1655b5584ff77ec
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-10-01 21:16:38 +0100

    extract converter and api requests into seperate modules

--- stream.js
+++ stream.js
@@ -2,7 +2,8 @@ var http = require("http"),
     url = require("url"),
     path = require("path"),
     fs = require("fs"),
-    spawn = require('child_process').spawn,
+    Converter = require('./lib/converter'),
+    track = require('./lib/track'),
     pathRegex = /^\/stream\/(\d+)/;
 
 http.createServer(function (req, res) {
@@ -19,39 +20,20 @@ http.createServer(function (req, res) {
 
   var trackId = matcher[1]
 
-  var converter = spawn('ffmpeg', ['-i', 'pipe:0', '-f', 'mp3', '-acodec', 'libvorbis', '-aq', '60', '-f', 'ogg', '-'])
-
   res.writeHead(200, {"Content-Type": "application/ogg"})
 
-  converter.stdout.on('data', function (data) {
+  var converter = Converter.create()
+  
+  track.get(trackId, function (data) {
+    converter.send(data)
+  })
+
+  converter.onData(function (data) {
     res.write(data, 'binary')
   })
 
-  converter.stderr.on('data', function (data) {
-    console.log(data.toString('ascii'))
-  })
-
-  converter.on('exit', function (code) {
-    console.log('exited with code', code)
+  converter.onComplete(function () {
     res.end()
-  })
-
-  var options = {
-    host: "api.soundcloud.com",
-    path: "/tracks/" + trackId + "/stream?client_id=3bf80245493b4d50ce93429b9b8cfacb"
-  }
-
-  var apiRequest = http.get(options, function (res) {
-    var location = url.parse(res.headers.location)
-
-    var redirect = http.get({
-      host: location.host,
-      path: location.pathname + location.search
-    }, function (res) {
-      res.on('data', function (data) {
-        converter.stdin.write(data, 'binary')
-      })
-    })
   })
 
 }).listen(8004)
