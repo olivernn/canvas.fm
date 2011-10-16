948a121a8989909ba69b82e90a8ab6018f0f0928
Oliver Nightingale <oliver.n@new-bamboo.co.uk>
2011-10-16 14:24:03 +0100

    move client side routing into separate file

---
+++ public/javascripts/app.js
@@ -0,0 +1,24 @@
+var app = Davis(function () {
+
+  this.get('/', function (req) {
+    Track.recent()
+  })
+
+  this.get('/search', function (req) {
+    Track.search(req.params['query'])
+  })
+
+  this.get('/tracks/:track_id', function (req) {
+    Track.load(req.params['track_id'])
+  })
+
+  this.bind('start', function () {
+    audio.init()
+    canvas.init()
+    search.init()
+    trackControls.init()
+    trackIndex.init()
+  })
+})
+
+app.start()
